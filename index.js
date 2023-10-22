4ea296b7e557010a47e1e38a0ff7612c0b9cdc36
alvaro-67 <giustialvin123@gmail.com>
2023-10-22 18:57:32 -0300

    commit 10

--- index.js
+++ index.js
@@ -16,10 +16,16 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.json());
 
-app.post('/update-history', (req, res) => {
-  const updatedHistory = JSON.stringify(req.body);
-  fs.writeFileSync('history.json', updatedHistory);
-  res.sendStatus(200);
+app.post('/save', (req, res) => {
+  const calculations = req.body;
+  fs.writeFile('calculations.json', JSON.stringify(calculations), (err) => {
+      if (err) {
+          console.error(err);
+          res.status(500).json({ error: 'Error al guardar el archivo JSON.' });
+      } else {
+          res.json({ message: 'Archivo JSON actualizado exitosamente.' });
+      }
+  });
 });
 
 // Agregamos rutas al servidor
