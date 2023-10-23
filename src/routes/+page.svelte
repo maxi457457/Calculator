4cf17fd829409e8de84f725b19a3146ac82531ff
alvaro-67 <giustialvin123@gmail.com>
2023-10-23 08:29:26 -0300

    CSS casi terminado

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,105 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a class="btn" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="mx-auto overflow-hidden mt-10 shadow-lg mb-2 bg-purple-900 shadow-lg border rounded-lg lg:w-2/6 md:w-3/6 sm:w-4/6">
+    <div class="">
+      <div class="p-5 text-white text-center text-3xl bg-purple-900"><span class="text-orange-500">Calcu</span>lator</div>
+      <div class="p-5 pb-5 text-white text-right text-3xl bg-purple-800">
+        <input class="input input-ghost bg-purple-800" bind:value={pantalla}>
+      </div>
+      
+      
+ <div class="flex items-stretch bg-purple-900 h-24">
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">%</div>
+    </div>
+  
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">(</div>
+    </div>
+   
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">)</div>
+    </div>
+   
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">÷</div>
+    </div>
+</div>
+      
+<div class="flex items-stretch bg-purple-900 h-24">
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">7</div>
+    </div>
+  
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">8</div>
+    </div>
+   
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">9</div>
+    </div>
+   
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">×</div>
+    </div>
+</div>
+      
+<div class="flex items-stretch bg-purple-900 h-24">
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">4</div>
+    </div>
+  
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">5</div>
+    </div>
+   
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">6</div>
+    </div>
+   
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">-</div>
+    </div>
+</div>
+      
+<div class="flex items-stretch bg-purple-900 h-24">
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">1</div>
+    </div>
+  
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">2</div>
+    </div>
+   
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">3</div>
+    </div>
+   
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">+</div>
+    </div>
+</div>
+      
 
-asdasdasd
+<div class="flex items-stretch bg-purple-900 h-24 mb-4">
+    
+    <div class="flex-1 pl-7 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-52 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">0</div>
+    </div>
+   
+    <div class="flex-1 pl-7 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">.</div>
+    </div>
+   
+    <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
+      <div class="rounded-full h-20 w-20 flex items-center bg-orange-500 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">=</div>
+    </div>
+</div>
+      
+      
+  </div>
+</div>
+<script>
+    let pantalla = ""
+    function a(){
+
+    }
+</script>
